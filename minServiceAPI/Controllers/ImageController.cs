0b71ff75bf90a377d6d8fa366b44a1e2dfbd0ac8
jabir8210 <jabir8210@gmail.com>
2025-02-25 09:01:09 +0100

    Denne service er færdig

--- minServiceAPI/Controllers/ImageController.cs
+++ minServiceAPI/Controllers/ImageController.cs
@@ -1,25 +1,40 @@
-private string _imagePath = string.Empty;
-private readonly ILogger<ImageController> _logger;
-public ImageController(ILogger<ImageController> logger,
-IConfiguration configuration)
-{
-_logger = logger;
-_imagePath = configuration["ImagePath"] ?? String.Empty;
-}
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
-[HttpGet("listImages")]
-public IActionResult ListImages()
+namespace minServiceAPI.Controllers
 {
-List<Uri> images = new List<Uri>();
-if(Directory.Exists(_imagePath))
-{
-string [] fileEntries = Directory.GetFiles(_imagePath);
-foreach( var file in fileEntries)
-{
-var imageURI = new Uri(file, UriKind.RelativeOrAbsolute);
-images.Add(imageURI);
-}
-}
-return Ok(images);
-}
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private string _imagePath = string.Empty;
+        private readonly ILogger<ImageController> _logger;
 
+        public ImageController(ILogger<ImageController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _imagePath = configuration["ImagePath"] ?? string.Empty;
+        }
+
+        [HttpGet("listImages")]
+        public IActionResult ListImages()
+        {
+            List<Uri> images = new List<Uri>();
+
+            if (Directory.Exists(_imagePath))
+            {
+                string[] fileEntries = Directory.GetFiles(_imagePath);
+                foreach (var file in fileEntries)
+                {
+                    var imageURI = new Uri(file, UriKind.RelativeOrAbsolute);
+                    images.Add(imageURI);
+                }
+            }
+            return Ok(images);
+        }
+    }
+}
